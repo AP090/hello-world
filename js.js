41a10c99b353a23608fc197c6be1d4324ccb70d3
JaPNaA <leonehuang100@gmail.com>
2017-07-08 22:04:21 -0400

    Pretty comments

--- js.js
+++ js.js
@@ -1,4 +1,10 @@
-var a=document.getElementsByTagName('b')[0],i=0; //Get the "<b>" tag from the document, set it as "a"; Make a new number "i" and set it to 0
-setInterval(function(){ //Every 750 milliseconds,
-    a.innerHTML=" Hello. :) ".substr(0,++i); //set a's innerHTML to a section of " Hello. :) " with the length of i; increase i's value by 1;
-},750); //This where the 750 ms comes from
+var a=document.getElementsByTagName('b')[0],
+    i=0; 
+    /*
+     * Get the "<b>" tag from the document,
+     * and set it as "a"; Make a new number
+     * "i" and set it to 0
+     */
+setInterval(function(){                      // Every 750 milliseconds,
+    a.innerHTML=" Hello. :) ".substr(0,++i); // set a's innerHTML to a section of " Hello. :) " 
+},750);//(this is where the 750ms comes from)// with the length of i; Then, increase i's value by 1;
