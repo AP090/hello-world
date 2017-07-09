0f2d3b707afe8f9f7e78dac02eebcd3ab7d0451b
JaPNaA <leonehuang100@gmail.com>
2017-07-08 22:00:38 -0400

    Added comments.

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-var a=document.getElementsByTagName('b')[0],i=0;
-setInterval(function(){
-    a.innerHTML=" Hello. :) ".substr(0,++i);
-},750);
+var a=document.getElementsByTagName('b')[0],i=0; //Get the "<b>" tag from the document, set it as "a"; Make a new number "i" and set it to 0
+setInterval(function(){ //Every 750 milliseconds,
+    a.innerHTML=" Hello. :) ".substr(0,++i); //set a's innerHTML to a section of " Hello. :) " with the length of i; increase i's value by 1;
+},750); //This where the 750 ms comes from
